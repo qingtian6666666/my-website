d9b171a8e1f322226628562c873ef4f56e979703
BobSong <2434005232@qq.com>
2026-05-30 18:42:18 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import viteCompression from 'vite-plugin-compression'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  base: '/my-website/',
-})
 
 export default defineConfig({
   plugins: [
